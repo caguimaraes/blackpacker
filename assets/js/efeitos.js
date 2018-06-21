7846e7822c5cfb962493a6a1dcbb2c0f191e0502
caguimaraes <c.guimaraes88@yahoo.com>
2018-06-21 19:41:19 -0300

    Alteração Footer

---
+++ assets/js/efeitos.js
@@ -0,0 +1,26 @@
+// SCROLL DOWN
+
+$(document).ready(function() {
+    $(".btn-scroll").click(function() {
+        var href = $(this).attr( 'href' );
+         $('html, body').animate({
+             scrollTop: $(href).offset().top
+         }, 1000);
+     });
+});
+
+// SCROLL UP 
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {        
+        $('.arrow-up').fadeIn(200);    
+    } else {
+        $('.arrow-up').fadeOut(200);  
+        }
+});
+
+$(document).ready(function(){
+    $('.arrow-up').click(function(){
+        $('html, body').animate({scrollTop: 0}, 1000);
+    });
+});
